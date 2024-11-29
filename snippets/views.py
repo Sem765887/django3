a23abfd63b41f6cad85a714a14712b2262d87633
Sem765887 <semyon.gardt765887@mail.ru>
2024-11-30 00:27:08 +0700

    Итог

--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,8 @@
-from rest_framework import generics
-
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
@@ -19,18 +16,31 @@ def api_root(request, format=None):
    })
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                         IsOwnerOrReadOnly]
 
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-   queryset = Snippet.objects.all()
-   serializer_class = SnippetSerializer
-class SnippetHighlight(generics.GenericAPIView):
-   queryset = Snippet.objects.all()
-   renderer_classes = [renderers.StaticHTMLRenderer]
-
-   def get(self, request, **kwargs):
+   @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+   def highlight(self, request, *args, **kwargs):
        snippet = self.get_object()
        return Response(snippet.highlighted)
+
+   def perform_create(self, serializer):
+       serializer.save(owner=self.request.user)
+
+
+class UserList(generics.ListAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
