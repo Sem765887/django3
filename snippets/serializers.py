f7b567bc15920102afd7f0d2ecc286d6070ddf4b
Sem765887 <semyon.gardt765887@mail.ru>
2024-11-30 00:18:29 +0700

    Пункт 5

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -7,3 +7,10 @@ class SnippetSerializer(serializers.ModelSerializer):
    class Meta:
        model = Snippet
        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+class SnippetSerializer(serializers.ModelSerializer):
+   owner = serializers.ReadOnlyField(source='owner.username')
+
+   class Meta:
+       model = Snippet
+       fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
